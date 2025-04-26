f0f534858d0c22a79933421038d710e2284669bc
Yuhi-Yuhi1 <yuk.vanya@yandex.ru>
2025-04-26 16:32:53 +0300

    Framework was added

---
+++ EducationTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Framework
+{
+    [TestFixture]
+    public class EducationTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
